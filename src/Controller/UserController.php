58e7b99b33619e36f352ffdf2951bfbb48c72b45
Moz <mzaoui.2409@gmail.com>
2022-08-30 13:07:44 +0200

    - Installation des composants security-bundle, validator, form - Ajout de l'entité User - Contrainte sur les propriétés User - Ajout d'un formulaire pour enregistrer un utilisateur - Création de messages flash et CSS associé - Création des rôles

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserController extends AbstractController
+{
+    
+
+
+    
+}
