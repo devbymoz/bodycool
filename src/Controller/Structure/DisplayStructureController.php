ab5f814da500a3879f9f621115edf8efe2990db6
Moz <mzaoui.2409@gmail.com>
2022-09-25 12:36:55 +0200

    - Page structure unique - Modifier l’état d’une structure

--- src/Controller/Structure/DisplayStructureController.php
+++ src/Controller/Structure/DisplayStructureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Structure;
 
+use App\Entity\Franchise;
 use App\Entity\Structure;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,9 +25,9 @@ class DisplayStructureController extends AbstractController
      */
     #[Route('/mes-structures', name: 'app_mes_structures')]
     #[IsGranted('ROLE_FRANCHISE')]
-    public function myStructures(        
+    public function myStructures(
         ManagerRegistry $doctrine,
-       
+
     ): Response {
         // On récupère l'utisateur connecté et l'id de sa franchise.
         $user = $this->getUser();
@@ -46,6 +47,77 @@ class DisplayStructureController extends AbstractController
             'nbrDtructureDisable' => $nbrDtructureDisable
         ]);
     }
+
+
+
+
+    /**
+     * AFFICHE LES DÉTAILS D'UNE STRUCTURE
+     * 
+     * En lecture seul pour le role Franchise et Gestionnaire.
+     * En écriture pour le role Admin
+     * 
+     * @return Response
+     */
+    #[Route('/structure-{id<\d+>}', name: 'app_structure_unique')]
+    #[IsGranted('ROLE_GESTIONNAIRE')]
+    public function singleStucture(
+        $id,
+        Request $request,
+        ManagerRegistry $doctrine,
+    ): Response {
+        // Le franchisé peut accéder à toutes les structures de sa franchise.
+        // Le gestionnaire peut accéder à la structure qu'il gere.
+        // le technicien peut accéder à toutes les structures
+
+        $repo = $doctrine->getRepository(Structure::class);
+        $repoFranchise = $doctrine->getRepository(Franchise::class);
+
+        $structure = $repo->findOneBy(['id' => $id]);
+        if (empty($structure)) {
+            throw $this->createNotFoundException('Cette structure n\'existe pas.');
+        }
+
+        // On récupère la franchise à qui appartient la structure.
+        $franchise = $structure->getFranchise();
+
+        // On récupère le gestionnaire de la structure.
+        $userAdminStructure = $structure->getUserAdmin();
+
+        // On récupère le propriétaire de la franchise.
+        $userOwner = $structure->getFranchise()->getUserOwner();
+
+        // On récupère l'utilisateur connecté.
+        $userConnected = $this->getUser();
+
+        // On vérifie si l'utilisateur connecté est un admin.
+        $isAdmin = in_array('ROLE_ADMIN', $userConnected->getRoles());
+
+        // Si l'utilisateur connecté n'est pas le gestionnaire de la structure, le franchisé de la structure et un admin on interdit l'accès à la page.
+        if ($userConnected != $userOwner && $userConnected != $userAdminStructure && !$isAdmin) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas accéder à cette page');
+        }
+
+        // Si la structure est désactivé et que l'utilisateur connecté n'est pas un admin on interdit l'accès à la page.
+        if ($structure->isActive() === false && !$isAdmin) {
+            throw $this->createAccessDeniedException('Cette structure est désactivée');
+        }
+
+
+
+
+        return $this->renderForm('structure/single-structure.html.twig', [
+            'structure' => $structure,
+            'franchise' => $franchise,
+            'userAdminStructure' => $userAdminStructure
+
+
+
+        ]);
+    }
+
+
+
 
 
 }
